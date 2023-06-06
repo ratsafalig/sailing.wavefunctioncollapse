1904e04c0af539c22a23bf5a145a65d4b164e7ee
ratsafalig <ratsafalig@outlook.com>
2023-06-06 23:23:26 +0800

    update

---
+++ CollapseStrategy/NormalCollapseStrategy.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+namespace Sailing.WaveFunctionCollapse{
+    public class NormalCollapseStrategy : CollapseStrategy{
+
+    }
+}
